ba675dbdfc20ec1be551910a984c5ff4c7114bce
zitzmah <haleyzitzmann@gmail.com>
2023-12-16 12:44:46 -0700

    CRUD capability

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // import express
 const express = require("express")
+//import morgan
+const morgan = require("morgan")
+//import method override
+const methodOverride = require("method-override")
 
 // import our fruits
 // require will return the value of module.exports
@@ -15,7 +19,12 @@ app.use(express.static("public")) // use a "public" folder for files
 //expres.urlencoded (parse url encoded bodies)
 //adds the data to req.body
 app.use(express.urlencoded({ extended: true }))
-
+//morgan-log data about each request for debugging
+app.use(morgan("dev"))
+//methodOverride - allows us to override form post rquests
+//as a different method like PUT or DELETE
+//It will look for a _method url query
+app.use(methodOverride("_method"))
 
 // fruits index route
 // get request to /fruits
@@ -58,6 +67,50 @@ app.post("/fruits", (req, res) => {
     res.redirect("/fruits")
 })
 
+//DESTROY ROUTE - Deletes a Fruit
+//DELETE -> /fruits/:id
+//deletes the specified fruit, redirects to index
+app.delete("/fruits/:id", (req, res) => {
+    //get the id from params
+    const id = req.params.id
+    //then we'll splice it from the array
+    //arr.splice(index, numOfItemToCut)
+    fruits.splice(id, 1)
+    //redirect back to index
+    res.redirect("/fruits")
+})
+
+//EDIT ROUTE - Render a Form to Edit a specific fruit
+//GET to /fruits/:id/edit
+//Render a form with with the existing values filled in
+app.get("/fruits/:id/edit", (req, res) => {
+    //get the id from params
+    const id = req.params.id//get the fruit being updated
+    const fruit = fruits[id]
+    //send the id and the fruit over to the template
+    //edit.ejs -> ./views/edit.ejs
+    res.render("edit.ejs", { fruit, id })
+})
+
+//UDATE ROUTE - Receive the form dta, updates the fruit
+//PUT to /fruits/:id
+//Update teh specified fruit, then redirect to index
+app.put("/fruits/:id", (req, res) => {
+    //get the id
+    const id = req.params.id//get the body
+    const body = req.body
+    //convert readyToEat to true or false
+    if (body.readyToEat === "on") {
+        body.readyToEat = true
+    } else {
+        body.readyToEat = false
+    }
+    //swap the old version with the new version
+    fruits[id] = body
+    //redirect ack to the index page
+    res.redirect("/fruits")
+})
+
 // fruits show route
 // get request to /fruits/:id
 // return a single fruit
@@ -73,7 +126,7 @@ app.get("/fruits/:id", (req, res) => {
     // res.render(template, data)
     // for the template assume "/views/"
     // "show.ejs" =>  ./views/show.ejs
-    res.render("show.ejs", { fruit })
+    res.render("show.ejs", { fruit, id })
     // {fruit} is the same as {fruit:fruit}
 })
 
